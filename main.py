1805f9cba5e9379b3d39ee28e15fdb0e73e498d6
Md Monir Hossain Zitu <139119711+MONIR-HOSSAIN-ZITU@users.noreply.github.com>
2024-02-04 00:02:34 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# calculator
+
+x = input("Enter first number")
+y = input("Enter your second number")
+c1 = int(x) + int(y)
+c2 = int(x) - int(y)
+c3 = int(x) * int(y)
+c4 = int(x) / int(y)
+print("Here is your answer of",x,"+",y,"=",c1)
+print("Here is your answer of",x,"-",y,"=",c2)
+print("Here is your answer of",x,"*",y,"=",c3)
+print("Here is your answer of",x,"/",y,"=",c4)
